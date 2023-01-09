a5601a4926c5c5300756e16bc47395c42b2acefb
andres-saa <andrew19f@gmail.com>
2023-01-09 12:00:30 -0500

    algoritmos implementados falta heapsort

---
+++ algoritmos/bubbleSort.js
@@ -0,0 +1,25 @@
+
+export default function bubbleSort(array){
+    let flag = false;
+    for(var i = 0; i <= array.length-1; i++){
+        flag = false;
+        for(var j = 0; j < ( array.length - i -1); j++){
+
+            // Comparing two adjacent numbers 
+            // and see if first is greater than second
+            if(array[j] > array[j+1]){
+
+            // Swap them if the condition is true 
+            var temp = array[j]
+            array[j] = array[j + 1]
+            array[j+1] = temp
+            flag = true;
+            }
+        }
+        // console.log(array);
+        order.innerHTML= array
+        if(!flag){
+            console.log(array)
+            return;
+        }
+}}
