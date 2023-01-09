a516ff5d6e28215ff138e1c0b8ae1a9e71772b17
andres-saa <andrew19f@gmail.com>
2023-01-09 12:13:35 -0500

    boton de limpiar

--- algoritmos/bubbleSort.js
+++ algoritmos/bubbleSort.js
@@ -23,3 +23,4 @@ export default function bubbleSort(array){
             return;
         }
 }}
+
--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 const unorder = document.getElementById('arreglo')
 const order = document.getElementById('arregloOrdenado')
-
+const btn_limpiar=document.getElementById('limpiar')
 const arreglo = [ 1,4,7,9,6,445,3232,78,998,44,4,7,9,6,445,3232,78,998,44,4,7,9,6,445,3232,78,998,44,4,7,9,6,445,3232,78,998,44]
 let arregloOrdenado = []
 const btn_bubbleSort=document.getElementById('bubble')
@@ -20,6 +20,7 @@ unorder.innerHTML=arreglo
 
 //funciones de ordenamiento
 
+btn_limpiar.addEventListener('click',()=>{order.innerHTML=''},false)
 btn_bubbleSort.addEventListener('click',()=> { order.innerHTML=bubbleSort( arreglo ); } , false )
 btn_insertionSort.addEventListener('click',()=>{ order.innerHTML=insertionSort(arreglo); },false)
 btn_countingsort.addEventListener('click',()=>{ order.innerHTML=countingSort(arreglo,0,5000);},false)
